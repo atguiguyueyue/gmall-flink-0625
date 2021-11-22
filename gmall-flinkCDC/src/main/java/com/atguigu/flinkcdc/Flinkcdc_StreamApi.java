54231398c8e8e16cb60f0778aebeb0963ec1d95b
ZhangJY0426 <atguigu0426@gg.com>
2021-11-22 16:29:27 +0800

    day01下午

---
+++ gmall-flinkCDC/src/main/java/com/atguigu/flinkcdc/Flinkcdc_StreamApi.java
@@ -0,0 +1,51 @@
+package com.atguigu.flinkcdc;
+
+import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
+import com.alibaba.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.alibaba.ververica.cdc.debezium.DebeziumSourceFunction;
+import com.alibaba.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class Flinkcdc_StreamApi {
+    public static void main(String[] args) throws Exception {
+        //1.获取流的执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.setParallelism(1);
+
+        //2.Flink-CDC将读取binlog的位置信息以状态的方式保存在CK,如果想要做到断点续传,需要从Checkpoint或者Savepoint启动程序
+        //2.1 开启Checkpoint,每隔5秒钟做一次CK
+        env.enableCheckpointing(5000L);
+        //2.2 指定CK的一致性语义
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        //2.3 设置任务关闭的时候保留最后一次CK数据
+        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        //2.4 指定从CK自动重启策略
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 2000L));
+        //2.5 设置状态后端
+        env.setStateBackend(new FsStateBackend("hdfs://hadoop102:8020/flinkCDC"));
+        //2.6 设置访问HDFS的用户名
+        System.setProperty("HADOOP_USER_NAME", "atguigu");
+
+
+        DebeziumSourceFunction<String> sourceFunction = MySQLSource.<String>builder()
+                .hostname("hadoop102")
+                .port(3306)
+                .username("root")
+                .password("000000")
+                .databaseList("gmall_flink_0625")
+                .tableList("gmall_flink_0625.base_trademark")
+                //可选配置项,如果不指定该参数,则会读取上一个配置下的所有表的数据，注意：指定的时候需要使用"db.table"的方式
+                .startupOptions(StartupOptions.initial())
+                .deserializer(new StringDebeziumDeserializationSchema())
+                .build();
+
+        env.addSource(sourceFunction).print();
+
+        env.execute();
+    }
+}
